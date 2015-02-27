eaac03b631acc96c8fc75c7978d2da389f36f021
Kuvshinova Elena <lenochka_kuvshinova@mail.ru>
2015-02-27 13:55:24 +0300

    Initial commit

---
+++ src/Squarel.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Елена on 27.02.2015.
+ */
+/* Тестовая программа репозитория*/
+public class Squarel {
+    public static void main (String args[])
+    {
+        int a, b = 4, s;
+        a = 5;
+        s = a * b ;
+        System.out.println ("Площадь прямоугольника со сторонами 5 см и 4 см равна " + s + " квадратных сантиметров.");
+    }
+}
